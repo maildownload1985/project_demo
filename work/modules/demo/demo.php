d7b17fd8d2202f0f8ee95a2bdf85b8fba9a4384e
minh-tha <minh-tha@minhtha-PC>
2016-01-15 13:20:22 +0700

    up

---
+++ work/modules/demo/demo.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace work\modules\demo;
+
+class demo extends \yii\base\Module
+{
+    public $controllerNamespace = 'work\modules\demo\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
