cc356d137fe4de3e4b020942413d7fe455a114ef
haule <hauln@leverages.jp>
2016-01-16 12:04:32 +0700

    check all

--- work/modules/calendar/views/event/inc/_inviation.php
+++ work/modules/calendar/views/event/inc/_inviation.php
@@ -11,16 +11,21 @@ use kartik\widgets\ActiveForm;
 						<div class="container">
 							<h4><?= Yii::t('app', 'Department');?></h4>
 							<div class='col-md-6'>
-								<?= $form->field($model_department, 'id', ['template' => "{label}\n{input}\n{hint}\n{error}"])->checkboxList(Event::getDepartmentNameCheckBox())->label(false); ?>
+								<?= $form->field($model_department, 'id', ['template' => "{label}\n{input}\n{hint}\n{error}"])->checkboxList(Event::getDepartmentNameCheckBox(), ['id' => 'inviation_deparment_id'])->label(false); ?>
+								<div class="checkbox">
+								    <label>
+								    <input type="checkbox" id="checkAll">
+								    </label>
+							    </div>
 							</div>
 						</div>
-
+                        <!-- 
 						<div class="container">
 							<div class='col-md-6'>
 								<?= $form->field($model_department, 'id')->checkboxList(Event::getDepartmentNameCheckBox())->label(false); ?>
 							</div>
 						</div>
-
+                         -->
 						<div class="container">
 							<div class='col-md-6'>
 								<p>
